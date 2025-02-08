a83d4da382283b19bf4f239b6922d9626147d5eb
Mehak011463 <mehak3232.beai24@chitkara.edu.in>
2025-02-08 18:29:42 +0530

    Saving work before pull

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
@@ -6,6 +6,10 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__=="__main__":
     app.run(debug=True)
